c102b4b3e3ceea01cf7c5d99f2414f3f6c9f32bc
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-10-21 23:36:07 -0300

    Implement endpoint `Update` `[PUT]/items/:id`

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ app.get("/items", function (req, res) {
 
 // READ BY ID - [GET] /items/:id
 app.get("/items/:id", function (req, res) {
-  //Acessar o parâmetro de rota ID
-  //Subtrair 1 (um) para corrigir o índice (array) da lista que começa em 0 (zero)
+  // Acessar o parâmetro de rota ID
+  // Subtrair 1 (um) para corrigir o índice (array) da lista que começa em 0 (zero)
   const id = req.params.id - 1;
 
-  //Acesso ao item na lista a partir do index
+  // Acesso ao item na lista a partir do index
   const item = items[id]
 
-  //Exibe o item obtido
+  // Exibe o item obtido
   res.send(item)
 })
 
@@ -40,14 +40,29 @@ app.get("/items/:id", function (req, res) {
 })*/
 
 app.post("/items", function (req, res) {
-  //Extrair informação do corpo da requisição
+  // Extrair informação do corpo da requisição
   const item = req.body.name
 
-  //Inserir info na lista
+  // Inserir info na lista
   items.push(item)
 
-  //Envio msg de sucesso!
+  // Envio msg de sucesso!
   res.send("Item created successfully")
+})
+
+// UPDATE - [PUT] /items/:id
+app.put("/items/:id", function (req, res) {
+  // Acesso ao parâmetro de rota e corrigido o índice
+  const id = req.params.id - 1;
+
+  // Obtemos o novo item a partir do corpo da requisição
+  const newItem = req.body.name
+
+  // Colocando o novo item na mesma posição do item anterior
+  items[id] = newItem
+
+  // Envio msg de sucesso!
+  res.send("Item updated successfully.")
 })
 
 app.listen(3000, function () {
