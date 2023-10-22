63fcfb01d7f31821d5122dee1f691ae156a5f7f2
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-10-21 22:56:44 -0300

    Implement endpoint `Read All`

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+//CRUD de lista DevMon
+
+const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
+
+// READ ALL -[GET] /items
+
+app.get("/items", function (req, res) {
+  res.send(items)
+})
+
 app.listen(3000, function () {
   console.log("App running on http://localhost:3000");
 });
