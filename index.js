9dbe157833c58e3230e1954b98ba7f220c98a96b
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-10-22 00:24:13 -0300

    Replace list of strings to list of objects

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ app.get("/oi", function (req, res) {
 
 //CRUD de lista DevMon
 
-const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
+//const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
+const items = [
+  {
+    "id": 1,
+    "name": "Java",
+    "imageUrl": "https://salvatore.academy/devmon/1_java.png"
+  },
+  {
+    "id": 2,
+    "name": "Kotlin",
+    "imageUrl": "https://salvatore.academy/devmon/2_kotlin.png"
+  },
+]
 
 // READ ALL - [GET] /items
 
@@ -25,10 +37,16 @@ app.get("/items", function (req, res) {
 app.get("/items/:id", function (req, res) {
   // Acessar o parâmetro de rota ID
   // Subtrair 1 (um) para corrigir o índice (array) da lista que começa em 0 (zero)
-  const id = req.params.id - 1;
+  //const id = req.params.id - 1;
+
+  const id = +req.params.id
 
   // Acesso ao item na lista a partir do index
-  const item = items[id]
+  //const item = items[id]
+
+  const item = items.find(function (elemento) {
+    return elemento.id === id;
+  })
 
   // Exibe o item obtido
   res.send(item)
@@ -41,37 +59,62 @@ app.get("/items/:id", function (req, res) {
 
 app.post("/items", function (req, res) {
   // Extrair informação do corpo da requisição
-  const item = req.body.name
+  //const item = req.body.name
+  const item = req.body
+
+  // Adicionar id
+  item.id = items.length + 1
 
   // Inserir info na lista
   items.push(item)
 
   // Envio msg de sucesso!
-  res.send("Item created successfully")
+  //res.send("Item created successfully")
+
+  // Devolve objeto adiciona
+  res.send(item)
 })
 
 // UPDATE - [PUT] /items/:id
 app.put("/items/:id", function (req, res) {
   // Acesso ao parâmetro de rota e corrigido o índice
-  const id = req.params.id - 1;
+  //const id = req.params.id - 1;
+  const id = +req.params.id
 
   // Obtemos o novo item a partir do corpo da requisição
-  const newItem = req.body.name
+  //const newItem = req.body.name
+  const newItem = req.body
 
   // Colocando o novo item na mesma posição do item anterior
-  items[id] = newItem
+  //items[id] = newItem
+  const index = items.findIndex(function (elemento) {
+    return elemento.id === id
+  })
+
+  items[index] = {
+    ...items[index],
+    ...newItem
+  }
 
   // Envio msg de sucesso!
-  res.send("Item updated successfully.")
+ //res.send("Item updated successfully.")
+
+  // Devolve objeto adiciona
+  res.send(items[index])
 })
 
 // DELETE - [DELETE] /items/:id
 app.delete("/items/:id", function (req, res) {
   // Acesso ao parâmetro de rota e corrigido o índice
-  const id = req.params.id - 1;
+  //const id = req.params.id - 1;
+  const id = req.params.id;
 
+  const index = items.findIndex(function (element) {
+    return element.id === id
+  })
   // Removido a informação a partir do índice
-  delete items[id]
+  //delete items[id]
+  delete items[index]
 
   // Envio msg de sucesso!
   res.send("Item deleted successfully.")
