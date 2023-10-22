fd53e57e99d33ba574591d11af0fc98e9eab60d9
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-10-21 23:14:20 -0300

    Implement endpoint `CREATE`

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -13,10 +15,26 @@ app.get("/oi", function (req, res) {
 
 const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
 
-// READ ALL -[GET] /items
+// READ ALL - [GET] /items
 
 app.get("/items", function (req, res) {
   res.send(items)
+})
+
+// CREATE - [POST] /items
+/*app.post("/items", function (req, res) {
+  res.send("Create item")
+})*/
+
+app.post("/items", function (req, res) {
+  //Extrair informação do corpo da requisição
+  const item = req.body.name
+
+  //Inserir info na lista
+  items.push(item)
+
+  //Envio msg de sucesso!
+  res.send("Item created successfully")
 })
 
 app.listen(3000, function () {
